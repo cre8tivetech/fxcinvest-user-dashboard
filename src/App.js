d3968344a1687159738973269327790892bc9baa
Joshmatjjen <joshmatparrot@gmail.com>
2020-05-09 22:31:32 +0100

    feat: build my transfer page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./pages/dashboard/dashboard.component";
+import MyTransfers from "./pages/my-transfers/my-transfers.component";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Dashboard} />
-        {/* <Route exact path="/investment-plans" component={Investment} />
-        <Route exact path="/faq" component={Faq} />
-        <Route exact path="/register" component={Auth} />
-        <Route exact path="/signin" component={Auth} />
-        <Route path="/test-page" render={() => <p>Routing test page</p>} /> */}
+        <Route exact path="/my-transfers" component={MyTransfers} />
+        {/* <Route exact path="/faq" component={Faq} /> */}
+        {/* <Route exact path="/register" component={Auth} /> */}
+        {/* <Route exact path="/signin" component={Auth} /> */}
+        {/* <Route path="/test-page" render={() => <p>Routing test page</p>} /> */}
       </Switch>
     </div>
   );
---
+++ src/pages/my-transfers/my-transfers.component.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./my-transfers.styles.scss";
+import Message from "../../components/message/message.component";
+import Layout from "../../components/layout/layout.component";
+
+const MyTransfers = () => {
+  return (
+    <Layout>
+      <div className="my-transfers">
+        <Message />
+        <div className="my-transfers__title">
+          <h1>My Transfers</h1>
+        </div>
+        <div className="my-transfers__type">
+          <div className="my-transfers__type--box1">
+            <p>Transfer Type</p>
+            <select name="" id="">
+              <option value="">Select All</option>
+            </select>
+          </div>
+          <div className="my-transfers__type--box2">
+            <p>From</p>
+            <input type="date" name="" id="" />
+          </div>
+          <div className="my-transfers__type--box3">
+            <p>To</p>
+            <input type="date" name="" id="" />
+          </div>
+          <div className="my-transfers__type--box4 ripple1">
+            <div className="">
+              <i className="fa fa-search"></i>
+              <p>Search</p>
+            </div>
+          </div>
+        </div>
+        <div className="my-transfers__box">
+          <h1>Deposits</h1>
+          <div className="line"></div>
+          <p>You didn't make any transfer yet</p>
+          <div className="my-transfers__box--btn ripple1">Deposit Now</div>
+        </div>
+        <div className="my-transfers__box">
+          <h1>Withdrawals</h1>
+          <div className="line"></div>
+          <p>You didn't make any transfer yet</p>
+        </div>
+        <div className="my-transfers__box">
+          <h1>Internal transfers</h1>
+          <div className="line"></div>
+          <p>You didn't make any transfer yet</p>
+        </div>
+        <div className="my-transfers__withdrawals"></div>
+        <div className="my-transfers__internal"></div>
+      </div>
+    </Layout>
+  );
+};
+export default MyTransfers;
